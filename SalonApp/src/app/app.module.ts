a52cfd0ce8eb1bdea1db23e439017e467dc85ed3
Khalid Anwar <arqhhhe@gmail.com>
2018-06-05 17:17:04 +0500

    convert from webpact to cli

--- SalonApp/src/app/app.module.ts
+++ SalonApp/src/app/app.module.ts
@@ -1,13 +1,13 @@
-﻿import { NgModule }      from '@angular/core';
+﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule }    from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 // used to create fake backend
 import { fakeBackendProvider } from './_helpers';
 
-import { AppComponent }  from './app.component';
-import { routing }        from './app.routing';
+import { AppComponent } from './app.component';
+import { routing } from './app.routing';
 
 import { AlertComponent } from './_directives';
 import { AuthGuard } from './_guards';
@@ -15,7 +15,9 @@ import { JwtInterceptor } from './_helpers';
 import { AlertService, AuthenticationService, UserService } from './_services';
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
-import { RegisterComponent } from './register';
+import { RegisterComponent } from './register';;
+import { HeaderComponent } from './header/header.component';;
+import { ServiceComponent } from './service/service.component'
 
 @NgModule({
     imports: [
@@ -29,7 +31,9 @@ import { RegisterComponent } from './register';
         AlertComponent,
         HomeComponent,
         LoginComponent,
-        RegisterComponent
+        RegisterComponent,
+        HeaderComponent,
+        ServiceComponent
     ],
     providers: [
         AuthGuard,
